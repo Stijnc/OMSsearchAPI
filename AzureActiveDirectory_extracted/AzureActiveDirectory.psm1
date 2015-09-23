c19b98d33707d1f7bff44116176ec1fe490fdbf1
mfpgoose <tiandert@microsoft.com>
2015-09-23 14:22:51 +0200

    Extracted Zip files for viewing

---
+++ AzureActiveDirectory_extracted/AzureActiveDirectory.psm1
@@ -0,0 +1,349 @@
+﻿<#
+.SYNOPSIS 
+    Runs Azure AD Graph API REST calls
+    
+.DESCRIPTION
+    This function runs AD Graph API REST calls against Azure Active Direcotry. 
+
+.PARAMETER Username
+    Name of a user that has access to Azure Active Direcotry.
+    It should be of the format "user@contoso.onmicrosoft.com". Don't set this value if using application authentication
+
+.PARAMETER Password
+    Password that matches the Username. Set this if an AD user is used for authentication.
+    
+.PARAMETER AzureADDomain
+    The Azure Active Directory domain
+    It should be of the format contoso.onmicrosoft.com 
+
+.PARAMETER APIVersion
+    The version of the REST API to work against. Default is 2013-03-01
+
+.PARAMETER AppIdURI
+    The application URI you want to perform actions against. This will be included in the JWT token
+    that is returned. Default is https://management.core.windows.net/
+
+.PARAMETER ClientID
+    The Azure Active Directory client identifier for a registered application. 
+    The default for this is the well known PowerShell application client ID 1950a258-227b-4e31-a9cf-717495945fc2 that
+    will be used if a specific one is not passed in.
+
+.PARAMETER Secret
+    The secret associated with the Client ID application.
+   
+.PARAMETER Connection
+    A hashtable that contains the Username, Password, and AzureADDomain, ClientID, Secret, AppIDURI, APIVersion
+    
+.PARAMETER URI
+    A REST URI to run
+
+.PARAMETER Method
+    The REST method to use
+    Values can be "GET", "POST", "PUT", "DELETE", "PATCH"
+
+.PARAMETER Body
+    The body for the REST call
+    This parameter is only required if the method is a POST, PATCH, or PUT
+
+.EXAMPLE
+    # Get the list of users in Azure Active Direcotry
+    $URI = "https://graph.windows.net/contoso.onmicrosoft.com/users/"
+
+    Invoke-AzureADMethod -URI $URI -Method "GET" -Username "user@contoso.onmicrosoft.com" -Password "StrongPassword" -AzureADDomain "contoso.onmicrosoft.com"
+ 
+.EXAMPLE
+    # Create a new user
+    $URI = "https://graph.windows.net/contoso.onmicrosoft.com/users/"
+
+    # New AD user to create
+    $NewADUser = "TestUser"
+    $Password = "StrongPassword"
+    $AzureADDomain = "contoso.onmicrosoft.com"
+
+    # Create body content for the REST call with the new user
+$Body = @"
+{
+    "accountEnabled": true,
+    "displayName": "$NewADUser",
+    "mailNickname": "$NewADUser",
+    "passwordProfile": { "password" : "$Password", "forceChangePasswordNextLogin": false },
+    "userPrincipalName": "$NewADUser@$AzureADDomain"
+}
+"@
+
+    # Set up connection value to make it easier to pass in information to get access to Azure AD
+    $ADConnection = @{"Username"="user@contoso.onmicrosoft.com";"AzureADDomain"="contoso.onmicrosoft.com";"Password"="StrongPassword"}
+    
+    Invoke-AzureADMethod -URI $URI -Method "POST" -Body $Body -Connection $ADConnection
+
+.EXAMPLE
+    # Authenticate to Azure AD using client application registered in Azure AD and perform actions against the Azure Resource Manager
+    $ClientID = "Client ID of the application"
+    $Secret = "Secret Key of the application"
+    $AzureADDomain = "contoso.onmicrosoft.com"
+    $APIVersion = "2015-01-01"
+    $APPIdURI = "https://management.core.windows.net/"
+
+    # Set up connection value to make it easier to pass in information to get access to Azure AD
+    $ADConnection = @{"AzureADDomain"=$AzureADDomain;"Secret"=$Secret;"APPIdURI"=$APPIdURI;"ClientID"=$ClientID;"APIVersion"=$APIVersion}
+    
+    # Get a list of resources in ARM
+    $URI = "https://management.azure.com/subscriptions/$SubscriptionId/resources"
+   
+    Invoke-AzureADMethod -URI $URI -Method "GET" -Connection $ADConnection
+       
+.NOTES
+    AUTHOR: Eamon O'Reilly
+    LASTEDIT: May 14th, 2015 
+#>
+Function Invoke-AzureADMethod
+{
+    Param(
+        [Parameter(ParameterSetName='ADCredential', Position=0, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Username = $null,
+
+        [Parameter(ParameterSetName='ADCredential', Position=1, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Password,
+
+        [Parameter(ParameterSetName='ADCredential', Position=2, Mandatory=$True)]
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=2, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $AzureADDomain,
+
+        # Operate against the Azure API by default. Could go against Azure AD with https://graph.windows.net 
+        [Parameter(ParameterSetName='ADCredential', Position=2, Mandatory=$False)]
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=2, Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $AppIdURI = "https://management.core.windows.net/",
+
+        # Well know PowerShell Client ID is the default if an specific client is not specified
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=0, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ClientID = "1950a258-227b-4e31-a9cf-717495945fc2",
+
+        # Well know PowerShell Client ID is the default if an specific client is not specified
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=0, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Secret,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $APIVersion = "2013-03-01",
+
+        [Parameter(ParameterSetName='UseConnectionObject', Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Hashtable]
+        $Connection,
+
+        [Parameter(Mandatory=$True)]
+        [String]
+        $URI,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateSet('GET','POST','DELETE','PUT','PATCH')]
+        [String]
+        $Method,
+
+        [Parameter(Mandatory=$False)]
+        [String]
+        $Body
+        )
+
+        # Use connection if specific values were set
+        if ($Connection -ne $null) 
+        { 
+            if ($Connection.UserName -ne $null) { $Username = $Connection.UserName }
+            if ($Connection.Password -ne $null) { $Password = $Connection.Password }
+            $AzureADDomain = $Connection.AzureADDomain
+            if ($Connection.AppIdURI -ne $null) { $AppIdURI = $Connection.AppIdURI }
+            if ($Connection.ClientId -ne $null) { $ClientId = $Connection.ClientId }
+            if ($Connection.Secret -ne $null) { $Secret = $Connection.Secret }
+            if ($Connection.APIVersion -ne $null) { $APIVersion = $Connection.APIVersion }
+        }
+
+
+        # Get a JSON web token
+
+        # Use the UserName & Password to authenticate
+        if ($Username -ne $null -and $Username -ne "")
+        {
+            Write-Verbose ("Using Username and password for authentication." + "Username is : " + $Username)
+            $AuthHeader = Get-AzureADToken -Username $Username -Password $Password -AzureADDomain $AzureADDomain -AppIdURI $AppIdURI
+        }
+        # Else, we are authenticating against an applicaiton using the secret key for this applicaiton client id.
+        else
+        {
+            Write-Verbose ("Using application with secret key for authentication." + "Application ID is : " + $ClientID)
+            $AuthHeader = Get-AzureADToken -ClientID $ClientID -AzureADDomain $AzureADDomain -AppIdURI $AppIdURI -Secret $Secret
+        }
+    
+
+        $Header = @{
+        "x-ms-version" = "$APIVersion";
+        "Authorization" = $AuthHeader
+        }
+
+        # Add the API version
+        $URI = $URI + "?api-version=$APIVersion"
+
+        If ($Method -eq "GET")
+        {
+            Invoke-RestMethod -Uri $URI -Method $Method -Headers $Header
+        }
+        Else
+        {
+            Invoke-RestMethod -Uri $URI -Method $Method -Headers $Header -Body $Body -ContentType "application/json"
+        }
+}
+
+<#
+.SYNOPSIS 
+    Gets an Azure AD authentication token that can be used it future calls 
+    
+.DESCRIPTION
+    This cmdlet returns an Azure AD token that can be used when making calls against resources integrated with Azure Active Directory. 
+
+.PARAMETER Username
+    Name of a user that has access to Azure Active Direcotry.
+    It should be of the format "user@contoso.onmicrosoft.com". Don't set this value if using application authentication
+
+.PARAMETER Password
+    Password that matches the Username. Set this if an AD user is used for authentication.
+    
+.PARAMETER AzureADDomain
+    The Azure Active Directory domain
+    It should be of the format contoso.onmicrosoft.com 
+
+.PARAMETER AppIdURI
+    The application URI you want to perform actions against. This will be included in the JWT token
+    that is returned. Default is https://management.core.windows.net/. If you want to make calls against Azure AD directly,
+    then you can set this to https://graph.windows.net/
+
+.PARAMETER ClientID
+    The Azure Active Directory client identifier for a registered application. 
+    The default for this is the well known PowerShell application client ID 1950a258-227b-4e31-a9cf-717495945fc2 that
+    will be used if a specific one is not passed in.
+
+.PARAMETER Secret
+    The secret associated with the Client ID application.
+   
+.PARAMETER Connection
+    A hashtable that contains the Username, Password, and AzureADDomain, ClientID, Secret, AppIDURI
+    
+
+.EXAMPLE
+    # Get a token to run commands against the Azure AD Graph API.
+    $URI = "https://graph.windows.net/contoso.onmicrosoft.com/users/"
+
+    $Token = Get-AzureADToken -Username "user@contoso.onmicrosoft.com" -Password "StrongPassword" -AzureADDomain "contoso.onmicrosoft.com" -AppIdURI "https://graph.windows.net"
+    
+    $URI = "https://graph.windows.net/contoso.onmicrosoft.com/users?api-version=beta" 
+
+    $Header = @{
+        "x-ms-version" = "beta";
+        "Authorization" = $Token
+    }
+
+    Invoke-RestMethod -URI $URI -Headers $Header -Method GET
+ 
+
+       
+.NOTES
+    AUTHOR: Eamon O'Reilly
+    LASTEDIT: April 15th, 2015 
+#>
+Function Get-AzureADToken
+{
+    Param(
+        [Parameter(ParameterSetName='ADCredential', Position=0, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Username = $null,
+
+        [Parameter(ParameterSetName='ADCredential', Position=1, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Password,
+
+        [Parameter(ParameterSetName='ADCredential', Position=2, Mandatory=$True)]
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=2, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $AzureADDomain,
+
+        # Operate against the Azure API by default. Could go against Azure AD with https://graph.windows.net 
+        [Parameter(ParameterSetName='ADCredential', Position=2, Mandatory=$False)]
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=2, Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $AppIdURI = "https://management.core.windows.net/",
+
+        # Well know PowerShell Client ID is the default if an specific client is not specified
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=0, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ClientID = "1950a258-227b-4e31-a9cf-717495945fc2",
+
+        # Well know PowerShell Client ID is the default if an specific client is not specified
+        [Parameter(ParameterSetName='ClientApplicationCredential', Position=0, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Secret,
+
+        [Parameter(ParameterSetName='UseConnectionObject', Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Hashtable]
+        $Connection
+        )
+
+        # Use connection if specific values were set
+        if ($Connection -ne $null) 
+        { 
+            if ($Connection.UserName -ne $null) { $Username = $Connection.UserName }
+            if ($Connection.Password -ne $null) { $Password = $Connection.Password }
+            $AzureADDomain = $Connection.AzureADDomain
+            if ($Connection.AppIdURI -ne $null) { $AppIdURI = $Connection.AppIdURI }
+            if ($Connection.ClientId -ne $null) { $ClientId = $Connection.ClientId }
+            if ($Connection.Secret -ne $null) { $Secret = $Connection.Secret }
+        }
+
+
+        # Get a JSON web token
+
+        # Use the UserName & Password to authenticate
+        if ($Username -ne $null -and $Username -ne "")
+        {
+            Write-Verbose "Using Azure AD Credentials"
+            # Set up authority to the common authentication
+            $Authority = "https://login.windows.net/common"
+            $AuthContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $Authority, $False
+            $Creds = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserCredential" -ArgumentList $UserName, $Password
+            $ClientID = "1950a258-227b-4e31-a9cf-717495945fc2"
+            $AuthResult = $AuthContext.AcquireToken($AppIdURI, $ClientId, $Creds)
+        }
+        # Else, we are authenticating against an applicaiton using the secret key for this applicaiton client id.
+        else
+        {
+            Write-Verbose "Using Azure AD Application with secret key"
+            # Set up authority to the domain the application is part of.
+            $Authority = "https://login.windows.net/$AzureADDomain"
+
+            $AuthContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $Authority, $False
+            $Creds = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential" -ArgumentList $ClientID, $Secret
+            $AuthResult = $AuthContext.AcquireToken($AppIdURI, $Creds)
+        }
+
+       
+        # Return token
+        $AuthResult.CreateAuthorizationHeader()
+
+}
---
+++ OperationsManagementSuite_extracted/OperationsManagementSuite.psm1
@@ -0,0 +1,212 @@
+﻿<#
+.SYNOPSIS 
+    Gets the Operations Management Suite workspace or all workspace if one is not specified
+    
+.DESCRIPTION
+    This cmdlet retrives all of the Operations Mangement Suite workspaces or a specific
+    one if specified
+
+.PARAMETER Token
+    The Azure AD token that has access to the OMS workspace. Use the AzureActiveDirectory module on 
+    ScriptCenter to retrieve the token by calling the Get-AzureADToken cmdlet
+
+.PARAMETER SubscriptionID
+    Subscription ID that the OMS workspace is in
+
+.PARAMETER Region
+    The name of the region that the OMS workspace is in. Examples are East-US, West-Europe
+    
+.PARAMETER Workspace
+    The name of the workspace to retrieve information about. Optional Parameter
+
+.PARAMETER APIVersion
+    The API version to use for OMS Queries. Default is 2014-10-10. Optional Parameter
+    
+.PARAMETER Connection
+    A hashtable that contains the SubscriptionID, Region, Workspace, and APIVersion
+
+.EXAMPLE
+    # Get list of all workspaces in a subscription for a particular region
+
+    # Get the authentication token from Azure AD. Can use the AzureActiveDirectory Module example on
+    # ScriptCenter or a different call to get the token. 
+    $Token = Get-AzureADToken -Connection $ADConnection
+
+    Get-OMSWorkspace -Token $Token -SubscriptionID "bccac418-bb95-422e-8e83-ddb7060aa359" -Region "East-US"
+ 
+.EXAMPLE
+    # Get a specific workspace in a subscription for a particular region
+    
+    # Get the authentication token from Azure AD. Can use the AzureActiveDirectory Module example on
+    # ScriptCenter or a different call to get the token. 
+    $Token = Get-AzureADToken -Connection $ADConnection
+
+    # Set up the connection object for OMS
+    $OMSConnection = @{"WorkSpace"="contoso";"SubscriptionID"="bccac418-bb95-422e-8e83-ddb7060aa359";"Region"="East-US";"APIVersion"="2014-10-10"}
+
+    Get-OMSWorkspace -Token $Token -Connection $OMSConnection
+        
+.NOTES
+    AUTHOR: Eamon O'Reilly
+    LASTEDIT: May 16th, 2015 
+#>
+Function Get-OMSWorkspace {
+    Param(
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Token,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $SubscriptionID,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Workspace,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Region,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [String] $APIVersion = "2014-10-10",
+
+        [Parameter(ParameterSetName='UseConnectionObject', Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [Hashtable]
+        $Connection
+
+    )
+
+    # Use connection if specific values were set
+    if ($Connection -ne $null) 
+    { 
+        if ($Connection.SubscriptionID -ne $null) { $SubscriptionID = $Connection.SubscriptionID }
+        if ($Connection.Workspace -ne $null) { $WorkSpace = $Connection.Workspace }
+        if ($Connection.Region -ne $null) { $Region = $Connection.Region }
+        if ($Connection.APIVersion -ne $null) { $APIVersion = $Connection.APIVersion }
+    }
+
+    $Url = "https://management.azure.com/subscriptions/$SubscriptionID/resourcegroups/OI-Default-$Region/providers/Microsoft.OperationalInsights"
+    
+    # Add workspace if one is specified, otherwise return all workspaces
+    if ($WorkSpace -ne "") { $Url = "${Url}/workspaces/" + "$Workspace"}
+    else { $Url = "${Url}/workspaces"}
+
+    # Add version to URL
+    $Url = $Url + "?api-version=$APIVersion"
+    Write-Verbose ("OMS URL is: " + $Url)
+
+    $Header = @{"Authorization"="${Token}"}
+
+    $Result = Invoke-RestMethod -Method Get -Headers $Header -Uri $Url
+
+    # Return array values to make them more PowerShell friendly
+    if ($Result.value -ne $null) {$Result.value}
+    else {$Result}
+}
+
+<#
+.SYNOPSIS 
+    Searches the Operations Management Suite workspace with a specific query
+    
+.DESCRIPTION
+    This cmdlet searches all of the Operations Mangement Suite workspace with a specific query
+
+.PARAMETER Token
+    The Azure AD token that has access to the OMS workspace. Use the AzureActiveDirectory module on 
+    ScriptCenter to retrieve the token by calling the Get-AzureADToken cmdlet
+
+.PARAMETER SubscriptionID
+    Subscription ID that the OMS workspace is in
+
+.PARAMETER Region
+    The name of the region that the OMS workspace is in. Examples are East-US, West-Europe
+    
+.PARAMETER Workspace
+    The name of the workspace to retrieve information about. Optional Parameter
+
+.PARAMETER APIVersion
+    The API version to use for OMS Queries. Default is 2014-10-10. Optional Parameter
+
+.PARAMETER Query
+    The Query to run against OMS. You can copy the query from the OMS search box
+    
+.PARAMETER Connection
+    A hashtable that contains the SubscriptionID, Region, Workspace, and APIVersion
+
+.EXAMPLE
+    # Get list of all error alerts that are not closed
+
+    # Get the authentication token from Azure AD. Can use the AzureActiveDirectory Module example on
+    # ScriptCenter or a different call to get the token. 
+    $Token = Get-AzureADToken -Connection $ADConnection
+
+    $Query = 'Type:Alert AlertSeverity:Error AlertState!=Closed'
+
+    # Set up the connection object for OMS
+    $OMSConnection = @{"WorkSpace"="contoso";"SubscriptionID"="bccac418-bb95-422e-8e83-ddb7060aa359";"Region"="East-US";"APIVersion"="2014-10-10"}
+
+    Search-OMSWorkspace -Token $Token -Query $Query -Connection $OMSConnection
+        
+.NOTES
+    AUTHOR: Eamon O'Reilly
+    LASTEDIT: May 16th, 2015 
+#>
+Function Search-OMSWorkspace {
+    Param(
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Token,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $SubscriptionID,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Workspace,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Query,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Region,
+
+        [Parameter(ParameterSetName='OMSInformation', Position=1, Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [String] $APIVersion = "2014-10-10",
+
+        [Parameter(ParameterSetName='UseConnectionObject', Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [Hashtable]
+        $Connection
+    )
+
+    # Use connection if specific values were set
+    if ($Connection -ne $null) 
+    { 
+        if ($Connection.SubscriptionID -ne $null) { $SubscriptionID = $Connection.SubscriptionID }
+        if ($Connection.WorkSpace -ne $null) { $Workspace = $Connection.Workspace }
+        if ($Connection.Region -ne $null) { $Region = $Connection.Region }
+        if ($Connection.APIVersion -ne $null) { $APIVersion = $Connection.APIVersion }
+    }
+
+    $Url = "https://management.azure.com/subscriptions/$SubscriptionID/resourcegroups/OI-Default-$Region/providers/Microsoft.OperationalInsights/"
+    $QueryUrl = "${Url}workspaces/" + $Workspace + "/search?api-version=$APIVersion"
+    Write-Verbose ("OMS URL is: " + $WorkspaceUrl)
+
+    $Header = @{"Authorization"="${Token}"}
+
+    $QueryBody = "{'Query':'$Query'}"
+    Write-Verbose ("OMS Query is: " + $QueryBody)
+
+    $Result = Invoke-RestMethod -Method Post -Headers $Header -Uri $QueryUrl -Body $QueryBody -ContentType "application/json"
+    
+    # Return array values to make them more PowerShell friendly
+    if ($Result.value -ne $null) {$Result.value}
+    else {$Result}
+}
